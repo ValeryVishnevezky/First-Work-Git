8255b1ae3526ff6cc369a1562c4601b32ddc2274
Valery Vishnevezky <valery020201@gmail.com>
2024-09-10 21:52:59 +0300

    Add parameters maxDiameter and ball to onBallClick and add the onBallClick to ball2 in html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-function onBallClick(){
-    const ball = document.querySelector('.ball')
+function onBallClick(ball, maxDiameter){
     const newSize = ball.offsetWidth + getRandomInt(20, 60)
     ball.style.width = ball.style.height = newSize + 'px'
     ball.textContent = newSize
     ball.style.backgroundColor = getRandomColor()
-    if (newSize > 400) {
+    if (newSize > maxDiameter) {
         ball.style.width = ball.style.height = 100 + 'px'
         ball.textContent = '100'
     }
