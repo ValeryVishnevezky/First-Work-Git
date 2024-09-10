5b266070bf60bac355b3875edd8cb8d43eb0196c
Valery Vishnevezky <valery020201@gmail.com>
2024-09-10 21:17:28 +0300

    Add getRandomColor to util and to onBallClick function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function onBallClick(){
     const newSize = ball.offsetWidth + getRandomInt(20, 60)
     ball.style.width = ball.style.height = newSize + 'px'
     ball.textContent = newSize
+    ball.style.backgroundColor = getRandomColor()
     if (newSize > 400) {
         ball.style.width = ball.style.height = 100 + 'px'
         ball.textContent = '100'
