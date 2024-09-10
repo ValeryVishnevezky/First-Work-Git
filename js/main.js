7d8f671c7ec879cd83430d1241bfb29f0c3c4b58
Valery Vishnevezky <valery020201@gmail.com>
2024-09-10 19:36:06 +0300

    Add a limit for ball size growing and reset to 100px

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 function onBallClick(){
-const ball = document.querySelector('.ball')
-const newSize = ball.offsetWidth + 50
-ball.style.width = ball.style.height = newSize + 'px'
-ball.textContent = newSize
+    const ball = document.querySelector('.ball')
+    const newSize = ball.offsetWidth + 50
+    ball.style.width = ball.style.height = newSize + 'px'
+    ball.textContent = newSize
+    if (newSize > 400) {
+        ball.style.width = ball.style.height = 100 + 'px'
+        ball.textContent = '100'
+    }
 }
