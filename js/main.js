cdec23d79f23ad62bd823151f8f2362356804a68
Valery Vishnevezky <valery020201@gmail.com>
2024-09-10 22:33:43 +0300

    Add ball3 and ball4 and function to reduce the balls size and change functions name to more readable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function onBallClick(ball, maxDiameter){
+function onBallClickIncrease(ball, maxDiameter){
     const newSize = ball.offsetWidth + getRandomInt(20, 60)
     ball.style.width = ball.style.height = newSize + 'px'
     ball.textContent = newSize
@@ -7,4 +7,29 @@ function onBallClick(ball, maxDiameter){
         ball.style.width = ball.style.height = 100 + 'px'
         ball.textContent = '100'
     }
+}
+
+function onBallClickIncreaseFirstTwo() {
+    const ball1 = document.querySelector('.ball1')
+    const ball2 = document.querySelector('.ball2')
+    onBallClickIncrease(ball1, 200)
+    onBallClickIncrease(ball2, 200)
+}
+
+function onBallClickReduce(ball, minDiameter){
+    const newSize = ball.offsetWidth - getRandomInt(20, 60)
+    ball.style.width = ball.style.height = newSize + 'px'
+    ball.textContent = newSize
+    ball.style.backgroundColor = getRandomColor()
+    if (newSize < minDiameter) {
+        ball.style.width = ball.style.height = 100 + 'px'
+        ball.textContent = '100'
+    }
+}
+
+function onBallClickReduceFirstTwo() {
+    const ball1 = document.querySelector('.ball1')
+    const ball2 = document.querySelector('.ball2')
+    onBallClickReduce(ball1, 100)
+    onBallClickReduce(ball2, 100)
 }
